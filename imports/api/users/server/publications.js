ae54c9213ce410af1bf6ce17f44d83033466e24b
calvin-evans <calvin@webantic.co.uk>
2016-05-25 16:04:50 +0100

    fixes bug in user pub

--- imports/api/users/server/publications.js
+++ imports/api/users/server/publications.js
@@ -1,18 +1,26 @@
 import { Meteor } from 'meteor/meteor';
 import { EJSON } from 'meteor/ejson';
 
-Meteor.publish('users', function () {
+Meteor.publish('users', function() {
 	var self = this,
 		cursorHandle,
 		cursor = Meteor.users.find();
 
 	cursorHandle = cursor.observeChanges({
-		added(id, fields) 	{ self.added('users', id, fields); },
-		changed(id, fields) { self.changed('users', id, fields); },
-		removed(id) 				{ self.removed('users', id); },
+		added(id, fields) {
+			self.added('users', id, fields);
+		},
+		changed(id, fields) {
+			self.changed('users', id, fields);
+		},
+		removed(id) {
+			self.removed('users', id);
+		},
 	});
 
 	self.ready();
 
-	self.onStop(cursorHandle.stop);
+	self.onStop(() => {
+		cursorHandle && cursorHandle.stop();
+	});
 });
